d1207b2351be2cfa8c2665167601cc6ac976b4b9
Vishal Raj <vishalraj5781@gmail.com>
2025-08-24 13:29:02 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,14 @@
+# Task 1.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Addition = ", a + b)
+print("Subtraction = ", a - b)
+print("Multiplication = ", a * b)
+print("Division = ", a / b)
+print()
+
+# Task 2.
+first_name = input("Enter first name: ")
+last_name = input("Enter last name : ")
+full_name = first_name + " " + last_name
+print(f"\nHello,{full_name}!  Welcome to the Python program.")
